4d42d64236b361151ed7bf4e079db1822febf182
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2022-10-15 17:22:40 +0200

    part 2 (not yet tested)

--- PA3/Lab3.py
+++ PA3/Lab3.py
@@ -39,6 +39,14 @@ class CustomSlice(EventMixin):
             # """ Please add your logic here """
         }
 
+        def mac_int_to_mac(mac_int):
+            assert 10 > mac_int >= 0  # Else not supported
+            return EthAddr("00:00:00:00:00:0" + str(mac_int))
+
+        def dpid_int_to_dpid(dpid_int):
+            assert 10 > dpid_int >= 0
+            return "00-00-00-00-00-0" + str(dpid_int)
+
         udp_config = {
             "port": 200,
             "dst_mac": EthAddr('00:00:00:00:00:05'),
@@ -62,18 +70,13 @@ class CustomSlice(EventMixin):
             "hosts_disallowed": [1]
         }
 
-        self.configs = [udp_config, http_config]
+        udp_config["hosts_disallowed"] = [mac_int_to_mac(host) for host in udp_config["hosts_disallowed"]]
+        http_config["hosts_disallowed"] = [mac_int_to_mac(host) for host in http_config["hosts_disallowed"]]
+
+        self.configs = {"udp": udp_config, "http": http_config}
 
         # Add the routes from the configs to the portmap
-        for config in self.configs:
-
-            def mac_int_to_mac(mac_int):
-                assert 10 > mac_int >= 0  # Else not supported
-                return EthAddr("00:00:00:00:00:0" + str(mac_int))
-
-            def dpid_int_to_dpid(dpid_int):
-                assert 10 > dpid_int >= 0
-                return "00-00-00-00-00-0" + str(dpid_int)
+        for config in self.configs.values():
 
             def get_portmap_key(dpid_int, src_mac_int, dst_mac, port):
                 dpid = dpid_int_to_dpid(dpid_int)
@@ -84,6 +87,13 @@ class CustomSlice(EventMixin):
                 for i in range(len(route) - 1):
                     key = get_portmap_key(route[i], host, config["dst_mac"], config["port"])
                     self.portmap[key] = dpid_int_to_dpid(route[i + 1])
+
+    def get_switch_port(self, current_switch_dpid, next_switch_dpid):
+        # check if both switches are in the adjacency map
+        if current_switch_dpid in self.adjacency and next_switch_dpid in self.adjacency[current_switch_dpid]:
+            return self.adjacency[current_switch_dpid][next_switch_dpid]
+        else:
+            return None
 
     def _handle_ConnectionUp(self, event):
         dpid = dpidToStr(event.dpid)
@@ -127,6 +137,16 @@ class CustomSlice(EventMixin):
             msg.in_port = event.port
             event.connection.send(msg)
 
+        def install_drop_rule(event, host_mac, protocol_port):
+            # drop any packages from the host_mac on the protocol_port
+            msg = of.ofp_flow_mod()
+            msg.idle_timeout = 10
+            msg.hard_timeout = 30
+            msg.match.dl_src = host_mac
+            msg.match.tp_dst = protocol_port
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
+            event.connection.send(msg)
+
         def forward(message=None):
             this_dpid = dpid_to_str(event.dpid)
 
@@ -138,20 +158,51 @@ class CustomSlice(EventMixin):
                           packet.dst, dpid_to_str(event.dpid), event.port)
 
                 try:
-                    """ Add your logic here """"
+                    """ Add your logic here """
+
+                    def handle_unicast_packet(config, packet, event):
+                        # Check if the host is allowed to access the service
+                        if packet.src in config["hosts_disallowed"]:
+                            log.debug("Host %s is not allowed to access the HTTP service", packet.src)
+                            install_drop_rule(event, packet.src, config["port"])
+                            # Don't forward the packet, just return
+                            return
+
+                        # Get the next switch
+                        next_switch = self.portmap[(this_dpid, packet.src, config["dst_mac"], config["port"])]
+                        next_switch_port = self.get_switch_port(this_dpid, next_switch)
+                        if next_switch_port is not None:
+                            install_fwdrule(event, packet, next_switch_port)
+                            log.debug("Forwarding packet to %s on port %d", next_switch, next_switch_port)
+                            # Forward the packet
+                            msg = of.ofp_packet_out()
+                            msg.actions.append(of.ofp_action_output(port=next_switch_port))
+                            msg.data = event.ofp
+                            msg.in_port = event.port
+                            event.connection.send(msg)
+                        else:
+                            log.debug("Port not discovered yet, flooding")
+                            flood()
+
+                    if tcpp is not None and tcpp.dstport == 80:
+                        tcp_config = self.configs["http"]
+                        handle_unicast_packet(tcp_config, packet, event)
+                    if udpp is not None and udpp.dstport == 200:
+                        udp_config = self.configs["udp"]
+                        handle_unicast_packet(udp_config, packet, event)
 
 
                 except AttributeError:
                     log.debug("packet type has no transport ports, flooding")
 
-                # flood and install the flow table entry for the flood
-                install_fwdrule(event, packet, of.OFPP_FLOOD)
+                    # flood and install the flow table entry for the flood
+                    install_fwdrule(event, packet, of.OFPP_FLOOD)
 
         forward()
 
 
 def launch():
-    # Ejecute spanning tree para evitar problemas con topologías con bucles
+    # Run spanning tree to avoid problems with looping topologies
     pox.openflow.discovery.launch()
     pox.openflow.spanning_tree.launch()
 
